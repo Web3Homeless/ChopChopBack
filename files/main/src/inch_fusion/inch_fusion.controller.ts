869837c71c99040252d2419d7f2ac8fb2cb5aacb
asimaranov <a.simaranov@gmail.com>
2024-09-22 02:16:59 +0800

    Send quote wrapped

--- files/main/src/inch_fusion/inch_fusion.controller.ts
+++ files/main/src/inch_fusion/inch_fusion.controller.ts
@@ -8,6 +8,7 @@ import {
   Delete,
 } from '@nestjs/common';
 import { NetworkEnum, SDK } from 'lib/inch';
+import { GetQuoteRequestDto } from './dto/get-quote-request.dto';
 
 (BigInt.prototype as any).toJSON = function () {
   return this.toString();
@@ -17,16 +18,16 @@ import { NetworkEnum, SDK } from 'lib/inch';
 export class InchFusionController {
   constructor() {}
 
-  @Get()
-  async getQuote() {
+  @Post()
+  async getQuote(@Body() getQuoteDto: GetQuoteRequestDto) {
     const sdk = new SDK({
       url: 'https://api.1inch.dev/fusion-plus',
       authKey: process.env.INCH_API_KEY!,
     });
 
     const params = {
-      srcChainId: NetworkEnum.ETHEREUM as any,
-      dstChainId: NetworkEnum.GNOSIS as any,
+      srcChainId: getQuoteDto.srcChainId,
+      dstChainId: getQuoteDto.dstChainId,
       srcTokenAddress: '0x6b175474e89094c44da98b954eedeac495271d0f',
       dstTokenAddress: '0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee',
       amount: '1000000000000000000000',
